3767be615a6b2a97a5078053582eb707a88b6a77
Mikhail Gribkov <mishacool4@gmail.com>
2023-07-27 13:01:00 +0300

    done homework 4

--- src/phone.py
+++ src/phone.py
@@ -2,19 +2,23 @@ from src.item import Item
 
 
 class Phone(Item):
-    def __init__(self, name, price, quantity, number_of_sim):
+    def __init__(self, name, price, quantity: int, number_of_sim: int):
         super().__init__(name, price, quantity)
-        self._number_of_sim = number_of_sim
+        if number_of_sim <= 0:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля")
+        else:
+            self._number_of_sim = number_of_sim
 
     @property
     def number_of_sim(self):
         return self._number_of_sim
 
     @number_of_sim.setter
-    def number_of_sim(self, num):
-        self._number_of_sim = num
+    def number_of_sim(self, num: int):
         if num <= 0:
             raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля")
+        else:
+            self._number_of_sim = num
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self._number_of_sim})"
@@ -23,4 +27,4 @@ class Phone(Item):
         if isinstance(self, Phone) and isinstance(other, Phone):
             return int(self.quantity) + int(other.quantity)
         else:
-            raise Exception
+            raise TypeError('Данные объекты невозможно сложить')
